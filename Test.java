8e8c497c663ed9461c682983ff8157b57781a97b
Jun <jkubo20@stuy.edu>
2020-10-29 16:29:41 -0400

    Triangle and getPerimeter

--- Test.java
+++ Test.java
@@ -2,12 +2,14 @@ class Test {
   public static void main(String[] args) {
     Point p1 = new Point(1, 2);
     Point p2 = new Point(7, 8);
+    Point p3 = new Point(5, 6);
     Point p1s = new Point(1, 2);
     System.out.println(p1.getX());
     System.out.println(p1.getY());
     System.out.println(p1.distanceTo(p2));
     System.out.println(p1.equals(p1s));
     System.out.println(p1.equals(p2));
-    
+    Triangle t1 = new Triangle(p1, p2, p3);
+    System.out.println(t1.getPerimeter());
   }
 }

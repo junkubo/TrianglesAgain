c33ab781f23db34d0e0e58297579d3a0516fee01
Jun <jkubo20@stuy.edu>
2020-10-29 16:09:32 -0400

    equals

--- Point.java
+++ Point.java
@@ -17,4 +17,7 @@ class Point {
   public double distanceTo(Point other) {
     return Math.sqrt(Math.abs(Math.pow(x - other.getX(), 2) + (Math.abs(Math.pow(y - other.getY(), 2)))));
   }
+  public boolean equals(Point other) {
+    return x == other.getX() && y == other.getY();
+  }
 }
--- Test.java
+++ Test.java
@@ -2,9 +2,12 @@ class Test {
   public static void main(String[] args) {
     Point p1 = new Point(1, 2);
     Point p2 = new Point(7, 8);
+    Point p1s = new Point(1, 2);
     System.out.println(p1.getX());
     System.out.println(p1.getY());
     System.out.println(p1.distanceTo(p2));
-
+    System.out.println(p1.equals(p1s));
+    System.out.println(p1.equals(p2));
+    
   }
 }
